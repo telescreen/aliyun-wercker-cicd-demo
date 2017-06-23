34a9ceaf9525f26579c6fa80d64b12218c9480fc
telescreen <hongha.bui@g.softbank.co.jp>
2017-06-23 16:43:50 +0900

    Update: fix bug

--- cicd.py
+++ cicd.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.debug = True
 
 SBWorldDate = datetime(2017,7,20)
-SEC_TO_DAY = 60 * 60 * 20  # 60 seconds / min * 60 min / hour * 24 hour
+SEC_TO_DAY = 60 * 60 * 24  # 60 seconds / min * 60 min / hour * 24 hour
 
 @app.route("/")
 def index():
